b451d7da5c4706fa453369eeb1244f847f14e7d9
Nonnyeee <akudikechinonye@gmail.com>
2022-09-08 20:31:39 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 #include<string.h>
 /**
  * main - Entry point
- * Description:prints"\"Programming is like building a multilingual puzzle"*
+ * Description:prints "\"Programming is like building a multilingual puzzle"*
  * Return:Always 0 (Success)
  */
 int main(void)
-}
+{
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
