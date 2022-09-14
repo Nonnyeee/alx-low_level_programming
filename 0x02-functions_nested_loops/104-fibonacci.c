487676fcb2d558798141a0ceef696a24083fb580
Nonnyeee <akudikechinonye@gmail.com>
2022-09-14 15:27:25 -0700

    Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -15,7 +15,7 @@ int main(void)
 	for (count = 0; count < 92; count++)
 	{
 		sum = fib1 + fib2;
-		printf("%\lu, ", sum);
+		printf("%lu, ", sum);
 
 		fib1 = fib2;
 		fib2 = sum;
@@ -30,7 +30,7 @@ int main(void)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
-		if (fib1_hhalf2 + fib2_half2 > 9999999999)
+		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
 			half2 %= 100000000000;
