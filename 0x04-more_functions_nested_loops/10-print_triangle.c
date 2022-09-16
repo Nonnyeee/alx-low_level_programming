d28f66b5c1574cfc59444802f3efee49e247a0ae
Nonnyeee <akudikechinonye@gmail.com>
2022-09-16 04:56:09 -0700

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle using the # character
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j < (size - i); j++)
+			_putchar(' ');
+		for (j--; j < size; j++)
+			_putchar('\n');
+	}
+	_putchar('\n');
+}
