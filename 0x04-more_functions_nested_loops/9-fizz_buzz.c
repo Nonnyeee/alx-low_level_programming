74684c7146131ce9aa9b35ebe60287e36fa7bb6c
Nonnyeee <akudikechinonye@gmail.com>
2022-09-16 15:31:52 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - function that checks for uppercase character
- * Return: 0
+ * main - prints Buzz each numbers of 3 and 5
+ * Return: Always 0
  */
 int main(void)
 {
