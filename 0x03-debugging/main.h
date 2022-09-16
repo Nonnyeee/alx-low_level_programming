dc65690cca37679673d9c882d1020b83ed7afa47
Nonnyeee <akudikechinonye@gmail.com>
2022-09-16 01:05:59 -0700

    Task 2

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H	
+#define MAIN_H
+
+#include <stdio.h>
+
+void poitive_to_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
