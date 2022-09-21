8ed43bdda789979bcf99c5382e4291dd51afbb2b
Nonnyeee <akudikechinonye@gmail.com>
2022-09-20 20:55:36 -0700

    Task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,19 +5,19 @@
  * print_array - prints an inputted number of elements of an array of intergers
  * @a: array of intergers
  * @n: number of elements to be printed
+ * Return: nothing
  */
 void print_array(int *a, int n)
 {
-	int index;
+	int inc;
 
-	for (index = 0; index < n; index++)
+	for (inc = 0; inc < n; inc++)
 	{
-		printf("%d", a[index]);
+		if (inc != n - 1)
+			printf("%d, ", a[inc]);
+		else
+			printf("%d", a[inc]);
 
-		if (index == n - 1)
-			continue;
-
-		printf(",");
 	}
-	printf("\n");
+	printf(10);
 }
