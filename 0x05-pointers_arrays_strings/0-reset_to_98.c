b0312b58cc23b09372f481edcc999c05ea1a38bd
Nonnyeee <akudikechinonye@gmail.com>
2022-09-20 17:18:08 -0700

    Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to a parameter and resets its value
+ * @n: input interger
+ * Return: nil
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
