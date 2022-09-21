cf1577db5547d133c4d8fa6d2cef0ddf1eb332d1
Nonnyeee <akudikechinonye@gmail.com>
2022-09-20 17:51:15 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one character out of two
+ * @str: the string containing characters
+ */
+void puts2(char *str)
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
