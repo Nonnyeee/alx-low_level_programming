28b4f125e493af70982853553083d729a801b6cb
Nonnyeee <akudikechinonye@gmail.com>
2022-09-23 19:57:33 -0700

    Task 8

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,24 +7,25 @@
  */
 char *cap_string(char *s)
 {
-	int count = 0, i;
-	int sep_words[] = {32 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i = 0, j;
+	char a[] = "\t\n,;.!?\"(){}";
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	while (*(s + i))
 	{
-		for (i = 0; i < 13; i++)
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
 		{
-			if (*(s + count) == sep_words[i])
+			if (i == 0)
+				*(s + i) -= 'a' - 'A';
+			else
 			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+						*(s + i) -= 'a' - 'A';
+				}
 			}
 		}
-		count++;
+		i++;
 	}
 	return (s);
 }
