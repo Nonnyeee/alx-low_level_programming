2f4da8b22dcb6db8046ccd7cdce2196dc295f798
Nonnyeee <akudikechinonye@gmail.com>
2022-09-23 18:37:23 -0700

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,9 +11,8 @@ char *string_toupper(char *str)
 	while (str[index])
 	{
 		if (str[index] >= 'a' && str[index] <= 'z')
-			str[i9ndex] -= 32;
+			str[index] -= 32;
 		index++;
 	}
-
 	return (str);
 }
